8f421a99296e53f3a840a0892efdac09dfc0e252
Ifam0605 <rahmanifam2005@gmail.com>
2025-07-01 13:58:59 +0530

    Create Q9 - Create Inverted Half Pyramid of Alphabets.c

---
+++ C_Programming/Assignments2/Q9 - Create Inverted Half Pyramid of Alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+
+{
+    int i,j;
+    char input;
+    printf("Enter The Alphabet Range: ");
+    scanf("%c",&input);
+
+    for(i=input; i>='A'; i=i-1)
+       {
+          for(j='A'; j<=i; j=j+1)
+            {
+                printf("%c",j);
+                //printf("%c",i);
+            }
+                printf("\n");
+       }
+}
