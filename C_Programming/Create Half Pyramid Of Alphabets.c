f2c251f4f15623a0d6543ecad8667841bdce68dd
Ifam0605 <rahmanifam2005@gmail.com>
2025-07-01 12:42:47 +0530

    C_Programming

---
+++ C_Programming/Create Half Pyramid Of Alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    char m ;
+    printf("Enter the number char: ");
+    scanf("%d", &n);
+    for (i = 1; i <=n; i++)
+    {
+        for (m = 'A'; m < 'A' + i; m++)
+
+            {
+            printf("%c ", m);
+             }
+        printf("\n");
+    }
+
+}
