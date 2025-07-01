40b41516477514efa4e9133be983702c50af4311
Ifam0605 <rahmanifam2005@gmail.com>
2025-07-01 13:45:11 +0530

    Create Q1- Generate Even Numbers by Using Loop Statment.c

---
+++ C_Programming/Assignments2/Q1- Generate Even Numbers by Using Loop Statment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+
+{
+    int num,i;
+    printf("Enter a Number:");
+    scanf("%d",&num);
+
+    for(i=2; i<=num; i=i+2)
+    {
+        printf("%d\n",i);
+    }
+}
